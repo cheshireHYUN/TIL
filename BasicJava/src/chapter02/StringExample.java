517fb89bcacb81a0dc30021f005e06960542670a
cheshirehyun <seohyun@chungbuk.ac.kr>
2022-02-07 19:10:03 +0900

    Valuable

---
+++ BasicJava/src/chapter02/StringExample.java
@@ -0,0 +1,10 @@
+package chapter02;
+
+public class StringExample {
+	public static void main (String[] args) {
+		String name = "홍길동";
+		System.out.print(name);
+		String descriptipon = "홍길동전은 \"소설\"입니다";
+		System.out.print(descriptipon);
+	}
+}
